25c2a2534a53b8b0138d31aba44797b3827afb2c
Opusidea <opusidea@gmail.com>
2022-02-22 10:09:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const mongoose = require('mongoose')
 const app = express()
 
 // port
-const port = 4000
+// si la variable d'env APP_PORT n'est pas fournie, l'application écoutera le port 4000
+const APP_PORT = process.env.APP_PORT || 4000
+
+// mongo host
+// si la variable d'env MONGO_HOST n'est pas fournie, le client mongoose cherchera à se connecter sur localhost
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost'
 
 // middleware
 app.use(express.static('public'))
@@ -19,7 +24,7 @@ app.use(bodyParser.json())
 
 // connexion à mongodb
 // par défaut, le client mongoose se connecte sur le port 27017 (port écouté par le serveur mongodb)
-mongoose.connect('mongodb://localhost/nuvolapp', { useNewUrlParser: true })
+mongoose.connect('mongodb://' + MONGO_HOST + '/nuvolapp', { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, '[-] connection to mongdb NOT OK'))
 db.once('open', () => console.log('[+] connection to mongodb OK'))
@@ -73,6 +78,6 @@ app.delete('/expense/:id', (req, res) => {
     .catch(err => res.json({message: 'échec'}))
 })
 
-app.listen(port, () => {
-  console.log('[+] express webserver running on port ' + port + '...')
+app.listen(APP_PORT, () => {
+  console.log('[+] express webserver running on port ' + APP_PORT + '...')
 })
